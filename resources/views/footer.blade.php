7e0fef53b704e317c68f0d2bd68aa8982291e2db
jean luc kawel <20ka208@esisalama.org>
2024-01-14 16:33:02 +0200

    index

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,113 @@
+<!-- Footer Section Start -->
+<div class="section footer-section" style="background-image: url(assets/images/bg/footer-bg.jpg);">
+
+    <div class="container">
+        <!-- Footer Widget Wrap Start -->
+        <div class="footer-widget-wrap">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <!-- Footer Widget Start -->
+                    <div class="footer-widget-about">
+                        <a class="footer-logo" href="index.html"><img src="assets/images/log-white.png" alt="Logo"></a>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing eiciendis saepe ut vel voluptatibus! Culpa, laboriosam.   </p>
+{{--                        <div class="footer-social">--}}
+{{--                            <ul class="social">--}}
+{{--                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>--}}
+{{--                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>--}}
+{{--                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+                    </div>
+                    <!-- Footer Widget End -->
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <!-- Footer Widget Start -->
+                    <div class="footer-widget">
+                        <h4 class="footer-widget-title">Useful Links</h4>
+
+                        <div class="widget-link">
+                            <ul class="link">
+                                <li><a href="#">Terms & Conditions</a></li>
+                                <li><a href="#">About Company</a></li>
+                                <li><a href="#">Payment Gatway</a></li>
+                                <li><a href="#">Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Footer Widget End -->
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <!-- Footer Widget Start -->
+                    <div class="footer-widget">
+                        <h4 class="footer-widget-title">Our Services</h4>
+
+                        <div class="widget-link">
+                            <ul class="link">
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">Outsourcing</a></li>
+                                <li><a href="#">Networking</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Footer Widget End -->
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <!-- Footer Widget Start -->
+                    <div class="footer-widget">
+                        <h4 class="footer-widget-title">Contact Information</h4>
+
+                        <div class="widget-info">
+                            <ul>
+                                <li>
+                                    <div class="info-icon">
+                                        <i class="flaticon-phone-call"></i>
+                                    </div>
+                                    <div class="info-text">
+                                        <span><a href="#">+243 974 453 545</a></span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="info-icon">
+                                        <i class="far fa-envelope-open"></i>
+                                    </div>
+                                    <div class="info-text">
+                                        <span><a href="#">info@safetyio.net</a></span>
+                                    </div>
+                                </li>
+{{--                                <li>--}}
+{{--                                    <div class="info-icon">--}}
+{{--                                        <i class="flaticon-pin"></i>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="info-text">--}}
+{{--                                        <span>60 East 65th Street, NY</span>--}}
+{{--                                    </div>--}}
+{{--                                </li>--}}
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Footer Widget End -->
+                </div>
+            </div>
+        </div>
+        <!-- Footer Widget Wrap End -->
+    </div>
+
+    <!-- Footer Copyright Start -->
+    <div class="footer-copyright-area">
+        <div class="container">
+            <div class="footer-copyright-wrap">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <!-- Footer Copyright Text Start -->
+                        <div class="copyright-text text-center">
+                            <p>© Copyrights {{ now()->year }}  All rights reserved. </p>
+                        </div>
+                        <!-- Footer Copyright Text End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Copyright End -->
+</div>
+<!-- Footer Section End -->
---
+++ resources/views/index.blade.php
@@ -0,0 +1,322 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from thepixelcurve.com/html/techwix/techwix/blog.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 Jan 2024 16:41:17 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Techwix - Technology IT Solutions Consultancy HTML5 Template</title>
+    <meta name="robots" content="noindex, follow"/>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/all.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/flaticon.css">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/aos.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+    <!-- <link rel="stylesheet" href="assets/css/vendor/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css"> -->
+
+</head>
+
+<body>
+
+<div class="main-wrapper">
+
+
+    <!-- Preloader start -->
+    <div id="preloader">
+        <div class="preloader">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Start  -->
+    <div id="header" class="section header-section">
+
+        <div class="container">
+
+            <!-- Header Wrap Start  -->
+            <div class="header-wrap">
+
+                <div class="header-logo">
+                    <a href="index.html"><img src="assets/images/can.jpeg"  alt=""></a>
+                </div>
+
+                <div class="header-menu d-none d-lg-block">
+                    <ul class="main-menu">
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+
+                <!-- Header Meta Start -->
+                <div class="header-meta">
+                    <!-- Header Cart Start -->
+                    <div class="header-cart dropdown">
+                        <button class="cart-btn" data-bs-toggle="dropdown">
+                            <i class="flaticon-shopping-cart"></i>
+                            <span class="count">0</span>
+                        </button>
+                    </div>
+
+
+                    <div class="header-btn d-none d-xl-block">
+                        <a class="btn" href="login-register.html">Get Started</a>
+                    </div>
+                    <!-- Header Toggle Start -->
+                    <div class="header-toggle d-lg-none">
+                        <button data-bs-toggle="offcanvas" datza-bs-target="#offcanvasExample">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </button>
+                    </div>
+                    <!-- Header Toggle End -->
+                </div>
+                <!-- Header Meta End  -->
+
+            </div>
+            <!-- Header Wrap End  -->
+
+        </div>
+    </div>
+    <!-- Header End -->
+
+    <!-- Offcanvas Start-->
+    <div class="offcanvas offcanvas-start" id="offcanvasExample">
+        <div class="offcanvas-header">
+            <!-- Offcanvas Logo Start -->
+            <div class="offcanvas-logo">
+                <a href="index.html"><img src="assets/images/logo-white.png" alt=""></a>
+            </div>
+            <!-- Offcanvas Logo End -->
+            <button type="button" class="close-btn" data-bs-dismiss="offcanvas"><i class="flaticon-close"></i></button>
+        </div>
+
+        <!-- Offcanvas Body Start -->
+        <div class="offcanvas-body">
+            <div class="offcanvas-menu">
+                <ul class="main-menu">
+                    <li>
+                        <a href="index.html">Home</a>
+
+                    </li>
+                    <li>
+                        <a href="about.html">Aboute Us</a>
+                    </li>
+
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Offcanvas Body End -->
+    </div>
+    <!-- Offcanvas End -->
+
+
+    <!-- Page Banner Start -->
+    <div class="section page-banner-section" style="background-image: url(assets/images/bg/page-banner.jpg);">
+        <div class="shape-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="944px" height="894px">
+                <defs>
+                    <linearGradient id="PSgrad_0" x1="88.295%" x2="0%" y1="0%" y2="46.947%">
+                        <stop offset="0%" stop-color="rgb(67,186,255)" stop-opacity="1"/>
+                        <stop offset="100%" stop-color="rgb(113,65,177)" stop-opacity="1"/>
+                    </linearGradient>
+
+                </defs>
+                <path fill-rule="evenodd" fill="rgb(43, 142, 254)"
+                      d="M39.612,410.76 L467.344,29.824 C516.51,-13.476 590.638,-9.93 633.938,39.613 L914.192,317.344 C957.492,366.50 953.109,440.637 904.402,483.938 L476.671,864.191 C427.964,907.492 353.376,903.109 310.76,854.402 L29.823,576.670 C-13.477,527.963 -9.94,453.376 39.612,410.76 Z"/>
+                <path fill="url(#PSgrad_0)"
+                      d="M39.612,410.76 L467.344,29.824 C516.51,-13.476 590.638,-9.93 633.938,39.613 L914.192,317.344 C957.492,366.50 953.109,440.637 904.402,483.938 L476.671,864.191 C427.964,907.492 353.376,903.109 310.76,854.402 L29.823,576.670 C-13.477,527.963 -9.94,453.376 39.612,410.76 Z"/>
+            </svg>
+        </div>
+        <div class="shape-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="726.5px" height="726.5px">
+                <path fill-rule="evenodd" stroke="rgb(255, 255, 255)" stroke-width="1px" stroke-linecap="butt"
+                      stroke-linejoin="miter" opacity="0.302" fill="none"
+                      d="M28.14,285.269 L325.37,21.217 C358.860,-8.851 410.655,-5.808 440.723,28.14 L704.777,325.36 C734.846,358.859 731.802,410.654 697.979,440.722 L400.955,704.776 C367.132,734.844 315.338,731.802 285.269,697.978 L21.216,400.954 C-8.852,367.132 -5.808,315.337 28.14,285.269 Z"/>
+            </svg>
+        </div>
+        <div class="shape-3">
+            <svg xmlns="http://www.w3.org/2000/svg" width="515px" height="515px">
+                <defs>
+                    <linearGradient id="PSgrad_0" x1="0%" x2="89.879%" y1="0%" y2="43.837%">
+                        <stop offset="0%" stop-color="rgb(67,186,255)" stop-opacity="1"/>
+                        <stop offset="100%" stop-color="rgb(113,65,177)" stop-opacity="1"/>
+                    </linearGradient>
+
+                </defs>
+                <path fill-rule="evenodd" fill="rgb(43, 142, 254)"
+                      d="M19.529,202.280 L230.531,14.698 C254.559,-6.661 291.353,-4.498 312.714,19.528 L500.295,230.531 C521.656,254.558 519.493,291.353 495.466,312.713 L284.463,500.295 C260.436,521.655 223.641,519.492 202.281,495.465 L14.699,284.462 C-6.660,260.435 -4.498,223.640 19.529,202.280 Z"/>
+                <path fill="url(#PSgrad_0)"
+                      d="M19.529,202.280 L230.531,14.698 C254.559,-6.661 291.353,-4.498 312.714,19.528 L500.295,230.531 C521.656,254.558 519.493,291.353 495.466,312.713 L284.463,500.295 C260.436,521.655 223.641,519.492 202.281,495.465 L14.699,284.462 C-6.660,260.435 -4.498,223.640 19.529,202.280 Z"/>
+            </svg>
+        </div>
+        <div class="shape-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="972.5px" height="970.5px">
+                <path fill-rule="evenodd" stroke="rgb(255, 255, 255)" stroke-width="1px" stroke-linecap="butt"
+                      stroke-linejoin="miter" fill="none"
+                      d="M38.245,381.102 L435.258,28.158 C480.467,-12.32 549.697,-7.964 589.888,37.244 L942.832,434.257 C983.23,479.466 978.955,548.697 933.746,588.888 L536.733,941.832 C491.524,982.23 422.293,977.955 382.103,932.745 L29.158,535.732 C-11.32,490.523 -6.963,421.293 38.245,381.102 Z"/>
+            </svg>
+        </div>
+        <div class="container">
+            <div class="page-banner-wrap">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!-- Page Banner Content Start -->
+                        <div class="page-banner text-center">
+                            <h2 class="title">Blog Grid</h2>
+                            <ul class="breadcrumb justify-content-center">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Blog Grid</li>
+                            </ul>
+                        </div>
+                        <!-- Page Banner Content End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Banner End -->
+
+    <!-- Blog Start -->
+    <div class="section techwix-blog-grid-section section-padding">
+        <div class="container">
+            <div class="techwix-blog-grid-wrap">
+                <div class="row">
+
+
+                    @foreach ($blogs as $blog)
+
+                        <div class="col-lg-4 col-md-6">
+                            <!-- Single Blog Start -->
+                            <div class="single-blog">
+                                <div class="blog-image">
+                                    <a href=""><img loading="lazy" src=" {{ asset('storage/' . $blog->image) }}" alt="Picture"></a>
+                                    <div class="top-meta">
+{{--                                      dateday and month--}}
+                                        @php
+                                            $date = substr($blog->created_at, 0, 10);
+                                            $dateDay = substr($date, 8, 2);
+                                             $month = [
+                                                '01' => 'Jan',
+                                                '02' => 'Feb',
+                                                '03' => 'Mar',
+                                                '04' => 'Apr',
+                                                '05' => 'May',
+                                                '06' => 'Jun',
+                                                '07' => 'Jul',
+                                                '08' => 'Aug',
+                                                '09' => 'Sep',
+                                                '10' => 'Oct',
+                                                '11' => 'Nov',
+                                                '12' => 'Dec',
+                                                ];
+                                                $dateMonth = $month[substr($date, 5, 2)];
+                               @endphp
+                                        <span class="date"><span>{{ $dateDay }}</span>{{ $dateMonth }}</span>
+                                    </div>
+                                </div>
+                                <div class="blog-content">
+                                    <div class="blog-meta">
+                                        <span><i class="fas fa-user"></i> <a href="#">{{ $blog->author }}</a></span>
+                                        <span><i class="fa-brands fa-readme"></i> {{ $blog->id  }}</span>
+                                    </div>
+                                    @php
+                                        $slug = substr($blog->slug, 0, 30);
+                                    @endphp
+                                    <h3 class="title"><a href="">{{ $slug }} </a></h3>
+                                    <div class="blog-btn">
+                                        <a class="blog-btn-link" href="">Lire Plus <i
+                                                class="fas fa-long-arrow-alt-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    @endforeach
+
+                </div>
+                <!-- Page Pagination Start -->
+                <div class="techwix-pagination">
+                    <ul class="pagination justify-content-center">
+                        <li><a href="#"><i class="fa fa-angle-left"></i></a></li>
+                        <li><a href="blog.html">1</a></li>
+                        <li><a class="active" href="blog.html">2</a></li>
+                        <li><a href="blog.html">3</a></li>
+                        <li><span>...</span></li>
+                        <li><a href="blog.html"><i class="fa fa-angle-right"></i></a></li>
+                    </ul>
+                </div>
+                <!-- Page Pagination End -->
+            </div>
+        </div>
+    </div>
+    <!-- Blog End -->
+
+
+    @include('footer')
+
+    <!-- back to top start -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
+        </svg>
+    </div>
+    <!-- back to top end -->
+
+</div>
+
+<!-- JS
+============================================ -->
+<script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="assets/js/plugins/popper.min.js"></script>
+<script src="assets/js/plugins/bootstrap.min.js"></script>
+
+<!-- Plugins JS -->
+<script src="assets/js/plugins/swiper-bundle.min.js"></script>
+<script src="assets/js/plugins/aos.js"></script>
+<script src="assets/js/plugins/waypoints.min.js"></script>
+<script src="assets/js/plugins/back-to-top.js"></script>
+<script src="assets/js/plugins/jquery.counterup.min.js"></script>
+<script src="assets/js/plugins/appear.min.js"></script>
+<script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+
+<!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+
+
+<!-- Main JS -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+
+<!-- Mirrored from thepixelcurve.com/html/techwix/techwix/blog.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 Jan 2024 16:41:23 GMT -->
+</html>
